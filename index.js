31fdef39607092e0a0e84d7ebffc8f310b57012e
abhi-aro <abhiaro27@gmail.com>
2022-11-15 12:42:42 +0530

    deployment test

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
+app.get("/test", (req, res) => {
+  res.json("Chal to raha hai");
+});
+
 app.listen("8000", () => {
   console.log("Server is running on port 8000");
 });
